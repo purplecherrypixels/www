0131d1b3729dae3e04d42a0b744919a5ec5a0189
A. Koepcke <akoepcke@users.noreply.github.com>
2020-10-11 15:03:28 +0200

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="bg-gray-800 h-screen flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
       <img src="img/cherry.png" className="h-32 m-8" />
-      <h1 className="text-white font-mono text-2xl">Coming soon</h1>
+      <h1 className="font-mono text-2xl text-white">Coming soon</h1>
     </div>
   );
 }
