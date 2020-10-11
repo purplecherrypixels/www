9d377e09a31abc5f6418acaabc774fbd7326cfca
A. Koepcke <akoepcke@users.noreply.github.com>
2020-10-11 16:58:14 +0200

    page specific heads

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
-      <img src="img/cherry.png" className="h-32 m-8" />
-      <h1 className="font-mono text-2xl text-white">Coming soon</h1>
-    </div>
+    <>
+      <Head>
+        <title key="title">Coming soon | purplecherrypixels</title>
+      </Head>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
+        <img src="img/cherry.png" className="h-32 m-8" />
+        <h1 className="font-mono text-2xl text-white">Coming soon</h1>
+      </div>
+    </>
   );
 }
