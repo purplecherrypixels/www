c98b90846b2ed2242dc50ff00e08d53ad3aa9ee6
A. Koepcke <akoepcke@users.noreply.github.com>
2020-10-11 13:55:00 +0200

    setup tailwindcss, typescript

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-300 h-screen flex items-center justify-center text-xl font-bold">
+      Hello
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  experimental: {
+    applyComplexClasses: true,
+    uniformColorPalette: true,
+    extendedSpacingScale: true,
+    defaultLineHeights: true,
+    extendedFontSizeScale: true,
+    darkModeVariant: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
