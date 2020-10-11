20611984c51e516c96a7d5eb083e7cf697ad72f8
A. Koepcke <akoepcke@users.noreply.github.com>
2020-10-11 15:03:39 +0200

    setup impressum

---
+++ pages/impressum.tsx
@@ -0,0 +1,7 @@
+export default function Impressum() {
+  return (
+    <div className="min-h-screen p-4 text-white bg-gray-800">
+      <h1 className="text-xl font-bold">Impressum</h1>
+    </div>
+  );
+}
