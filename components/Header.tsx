95bb904e032234092422618bd645a7851def110b
A. Koepcke <akoepcke@users.noreply.github.com>
2020-10-28 18:21:33 +0100

    WIP header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,10 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex items-end w-full h-20">
+      <img src="img/cherry.png" className="h-32 ml-8 -mb-2" />
+      <h1 className="ml-8 text-2xl text-white">purple cherry pixels</h1>
+    </div>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,6 @@ export default function Home() {
       <div className="font-sans bg-gray-900">
         <div className="flex flex-col items-center justify-between min-h-screen">
           <Header />
-          <div>
-            <img src="img/cherry.png" className="h-32 m-8" />
-            <h1 className="font-mono text-2xl text-white">Coming soon</h1>
-          </div>
           <Team />
           <div></div>
         </div>
